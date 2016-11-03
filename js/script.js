dbf7db30dbeb48ce0aea7edf22f452cb682b57df
NiloCortex <nilocortex@gmail.com>
2016-11-03 01:18:52 -0500

    First Draft: -  All images are in place -  Number order controlled via Jquery ( need a new system for changing this ) -  Animate.css used to make tubes slowly pulse -  Using Flex for responsive design -  El Psy Congroo

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+  var zero = $('#zero'),
+      one = $('#one'),
+      two = $('#two'),
+      three = $('#three'),
+      four = $('#four'),
+      five = $('#five'),
+      six = $('#six'),
+      seven = $('#seven'),
+      eight = $('#eight'),
+      nine = $('#nine'),
+      point = $('#point');
+
+$('.ReadingSteiner').append(one);
+$('.ReadingSteiner').append(point);
+$('.ReadingSteiner').append(zero);
+$('.ReadingSteiner').append(four);
+$('.ReadingSteiner').append(eight);
+$('.ReadingSteiner').append(five);
+$('.ReadingSteiner').append(nine);
+$('.ReadingSteiner').append(six);
+    console.log( zero);
+});
